ee382316447a98c6ee7762bf5258c405cf01304e
shakiful_islam_khan <skhan@schertech.com>
2024-10-01 12:50:43 +0600

    fixed routing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { StatusComponent } from './status/status.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/user_status', pathMatch: 'full' },
   {
-    path: 'user_status',
-    loadChildren: () =>
-      import('./status/status.module').then((m) => m.StatusModule),
+    path: '',
+    title: 'Home Page',
+    component: StatusComponent,
+    pathMatch: 'full',
   },
   {
-    path: 'user_status/details/:id',
+    path: 'details/:id',
     loadChildren: () =>
       import('./status/team-status/team-status.module').then(
         (m) => m.TeamStatusModule
       ),
   },
-  { path: '**', redirectTo: '/user_status' },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
